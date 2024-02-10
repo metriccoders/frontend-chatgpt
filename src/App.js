61c7be05095cabf21befb84256d99472d2fd9717
Metric Coders <techsaadhana@gmail.com>
2024-02-11 00:02:55 +0100

    add frontend code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Box from "@mui/material/Box";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import MenuIcon from "@mui/icons-material/Menu";
+import IconButton from "@mui/material/IconButton";
 
 function App() {
+  const [queryText, setQueryText] = useState("");
+  const [answer, setAnswer] = useState("");
+
+  const fetchAnswers = () => {
+    if( queryText.trim() !== "") {
+    setAnswer("Generating an answer...Please wait...");
+    axios
+      .post("http://127.0.0.1:80/answers/", {
+        query: queryText,
+      })
+      .then((res) => {
+        setAnswer(res.data);
+      });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1, fontWeight: "bold" }}>
+            Metric Coders - General GPT powered by Llama2
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <div style={{ margin: "20px", textAlign: "center" }}>
+        <div>
+          <TextField
+            onChange={(e) => setQueryText(e.target.value)}
+            style={{ width: "800px" }}
+            required
+            id="outlined-required"
+            label="Your question here..."
+            placeholder="Your question here..."
+          />
+          <Button
+            variant="contained"
+            style={{ margin: "10px", width: "200px" }}
+            onClick={fetchAnswers}
+          >
+            Submit
+          </Button>
+        </div>
+        <div style={{ textAlign: "justify", margin: "30px" }}>{answer}</div>
+      </div>
+    </Box>
   );
 }
 
